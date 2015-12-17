ad946b3bdbd83c80ad12d0b8978c6c603c9f319c
Dm430 <devinmwall@gmail.com>
2015-12-16 23:54:39 -0700

    Replaced typed line

--- POC/Program.cs
+++ POC/Program.cs
@@ -1,10 +1,9 @@
-﻿using System;
+﻿using Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using P2P;
-using Client;
 using UserModels;
 
 namespace POC {
@@ -16,33 +15,40 @@ namespace POC {
         }
 
         public Program() {
-            Console.WriteLine("Please enter a user name.");
-            string userName = Console.ReadLine();
+                Console.WriteLine("Please enter a user name.");
+                string userName = Console.ReadLine();
 
-            node = new P2PNode(new byte[] { 192, 168, 3, 234 });
-            node.RegisterListener(new ChatListener());
+                node = new P2PNode(new byte[] { 192, 168, 3, 234 });
+                node.RegisterListener(new ChatListener());
 
-            UserRegistration registration = new UserRegistration() {
-                UserName = userName,
-                PublicKey = "not implemented.................", // must be 32 characters long
-                Timestamp = DateTime.Now.ToBinary()
-            };
+                UserRegistration registration = new UserRegistration() {
+                    UserName = userName,
+                    PublicKey = "not implemented.................", // must be 32 characters long
+                    Timestamp = DateTime.Now.ToBinary()
+                };
 
-            node.RegisterUser(registration);
+                node.RegisterUser(registration);
 
+                StartChat();
+        }
+
+        private void StartChat() {
+            Console.Clear();
+            Console.WriteLine(".::Welcome to the chat::.");
             bool isChatting = true;
+
             while (isChatting) {
                 string message = Console.ReadLine();
 
                 if (message == "exit") {
                     isChatting = false;
                 } else {
-                    sendMessage(message);
+                    SendMessage(message);
                 }
             }
         }
 
-        private void sendMessage(string message) {
+        private void SendMessage(string message) {
             InstantMessage msg = new InstantMessage() {
                 Text = message,
                 UserName = "bob",
@@ -51,7 +57,12 @@ namespace POC {
             };
 
             node.Send(msg);
-            Console.WriteLine("You: " + message + "\n");
+            EchoMessage(message);
+        }
+
+        private void EchoMessage(string message) {
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.WriteLine("You: " + message);
         }
     }
 }
